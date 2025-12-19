e09e161aaba9960fb1cc02322c54ef366af981f0
Hengyu Ai <aihy2023@shanghaitech.edu.cn>
2025-12-20 01:35:40 +0800

    fix: deploy directory

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 wasm32-wasi-cabal build -f web
 obj_path=$(find ./dist-newstyle/ -name "*.wasm" -type f)
 frontend_obj_path=./frontend/public/wordle-helper.wasm
+mkdir -p frontend/public
 "$(wasm32-wasi-ghc --print-libdir)"/post-link.mjs -i "$obj_path" -o ./frontend/ghc_wasm_jsffi.js
 env -i GHCRTS=-H64m "$(type -P wizer)" --allow-wasi --wasm-bulk-memory true --inherit-env true --init-func _initialize -o $frontend_obj_path "$obj_path"
 wasm-opt --low-memory-unused --converge -O4 $frontend_obj_path -o $frontend_obj_path
